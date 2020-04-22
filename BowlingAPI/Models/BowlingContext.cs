9eaf135f7b0bdbfcb28337319695d4139b61dca3
Azeem Uraizee <aauraizee@gmail.com>
2020-04-21 22:25:22 -0400

    Added data context class

---
+++ BowlingAPI/Models/BowlingContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BowlingAPI.Models
+{
+    public class BowlingContext : DbContext
+    {
+        public BowlingContext(DbContextOptions<BowlingContext> options) : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Frame> Frames { get; set; }
+        public DbSet<Shot> Shots { get; set; }
+
+    }
+}
