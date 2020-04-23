7375c3facc74412a71055d91215e960c218f3aad
Azeem Uraizee <aauraizee@gmail.com>
2020-04-23 14:57:41 -0400

    Configured and created the database

---
+++ BowlingAPI/Migrations/20200423183514_InitialCreate.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BowlingAPI.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Players",
+                columns: table => new
+                {
+                    PlayerId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true),
+                    GamesPlayed = table.Column<int>(nullable: false),
+                    CurrentAverage = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Players", x => x.PlayerId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Games",
+                columns: table => new
+                {
+                    GameId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    TotalScore = table.Column<int>(nullable: false),
+                    PlayerId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games", x => x.GameId);
+                    table.ForeignKey(
+                        name: "FK_Games_Players_PlayerId",
+                        column: x => x.PlayerId,
+                        principalTable: "Players",
+                        principalColumn: "PlayerId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Frames",
+                columns: table => new
+                {
+                    FrameId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Value = table.Column<int>(nullable: false),
+                    TypeFlag = table.Column<int>(nullable: false),
+                    GameId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Frames", x => x.FrameId);
+                    table.ForeignKey(
+                        name: "FK_Frames_Games_GameId",
+                        column: x => x.GameId,
+                        principalTable: "Games",
+                        principalColumn: "GameId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Shots",
+                columns: table => new
+                {
+                    ShotId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    PinsHit = table.Column<int>(nullable: false),
+                    IsSpareShot = table.Column<bool>(nullable: false),
+                    SpareType = table.Column<string>(nullable: true),
+                    WasConverted = table.Column<bool>(nullable: false),
+                    FrameId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Shots", x => x.ShotId);
+                    table.ForeignKey(
+                        name: "FK_Shots_Frames_FrameId",
+                        column: x => x.FrameId,
+                        principalTable: "Frames",
+                        principalColumn: "FrameId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Frames_GameId",
+                table: "Frames",
+                column: "GameId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_PlayerId",
+                table: "Games",
+                column: "PlayerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Shots_FrameId",
+                table: "Shots",
+                column: "FrameId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Shots");
+
+            migrationBuilder.DropTable(
+                name: "Frames");
+
+            migrationBuilder.DropTable(
+                name: "Games");
+
+            migrationBuilder.DropTable(
+                name: "Players");
+        }
+    }
+}
