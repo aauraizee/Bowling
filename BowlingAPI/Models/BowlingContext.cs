fbb6343f6293515fbfec38c501e00532e1247c9c
Azeem Uraizee <aauraizee@gmail.com>
2020-04-23 13:59:04 -0400

    Defined OnModelCreating

--- BowlingAPI/Models/BowlingContext.cs
+++ BowlingAPI/Models/BowlingContext.cs
@@ -13,7 +13,13 @@ namespace BowlingAPI.Models
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<Player>().HasMany(p => p.Games).WithOne(g => g.Player);
+            modelBuilder.Entity<Game>().HasMany(g => g.Frames).WithOne(f => f.Game).HasForeignKey(f => f.GameId);
+            modelBuilder.Entity<Frame>().HasMany(f => f.Shots).WithOne(s => s.Frame).HasForeignKey(s => s.FrameId);
 
+            modelBuilder.Entity<Frame>()
+                .Property(f => f.TypeFlag)
+                .HasConversion<int>();
         }
 
         public DbSet<Player> Players { get; set; }
