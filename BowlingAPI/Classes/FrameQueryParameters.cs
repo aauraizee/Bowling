839504c60be78e667fb4c1b889e9c2bd429a397f
Azeem Uraizee <aauraizee@gmail.com>
2020-04-23 23:58:13 -0400

    Added query parameter and frame query parameter classes

---
+++ BowlingAPI/Classes/FrameQueryParameters.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BowlingAPI.Classes
+{
+    public class FrameQueryParameters : QueryParameters
+    {
+        public int Game { get; set; }
+        public int Value { get; set; }
+        public int Type { get; set; }
+    }
+}
