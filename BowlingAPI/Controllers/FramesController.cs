755f071c36f0e26df7749a8c4f07ce021d663fcd
Azeem Uraizee <aauraizee@gmail.com>
2020-04-23 23:36:18 -0400

    Added frame and shot controller

---
+++ BowlingAPI/Controllers/FramesController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BowlingAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BowlingAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class FramesController : ControllerBase
+    {
+
+        private readonly BowlingContext _context;
+
+        public FramesController(BowlingContext context)
+        {
+            _context = context;
+
+            _context.Database.EnsureCreated();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllFrames()
+        {
+            return Ok(await _context.Frames.ToArrayAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetFrame(int id)
+        {
+            var frame = await _context.Frames.FindAsync(id);
+            if (frame == null)
+            {
+                return NotFound();
+            }
+            return Ok(frame);
+        }
+    }
+}
---
+++ BowlingAPI/Controllers/ShotsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BowlingAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BowlingAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ShotsController : ControllerBase
+    {
+
+        private readonly BowlingContext _context;
+
+        public ShotsController(BowlingContext context)
+        {
+            _context = context;
+
+            _context.Database.EnsureCreated();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAllShots(int id)
+        {
+            var shot = await _context.Shots.FindAsync(id);
+            if (shot == null)
+            {
+                return NotFound();
+            }
+            return Ok(shot);
+        }
+    }
+}
