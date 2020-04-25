c2bb6f44c618b7fbcecc22054cd15484068083f2
Azeem Uraizee <aauraizee@gmail.com>
2020-04-25 12:59:33 -0400

    Added shot query parameter functionality

---
+++ BowlingAPI/Classes/ShotQueryParameters.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BowlingAPI.Classes
+{
+    public class ShotQueryParameters : QueryParameters
+    {
+        public int? Frame { get; set; }
+        public int? Pins { get; set; }
+        public bool? IsSpare { get; set; }
+        public bool? Converted { get; set; }
+        public string? SpareType { get; set; }
+    }
+}
--- BowlingAPI/Controllers/ShotsController.cs
+++ BowlingAPI/Controllers/ShotsController.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BowlingAPI.Classes;
 using BowlingAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BowlingAPI.Controllers
 {
@@ -22,8 +24,43 @@ namespace BowlingAPI.Controllers
             _context.Database.EnsureCreated();
         }
 
+        public async Task<IActionResult> GetAllShots([FromQuery] ShotQueryParameters queryParameters)
+        {
+            IQueryable<Shot> shots = _context.Shots;
+
+            if (queryParameters.Frame != null)
+            {
+                shots = shots.Where(
+                    s => s.FrameId == queryParameters.Frame);
+            }
+            if (queryParameters.Pins != null)
+            {
+                shots = shots.Where(
+                    s => s.PinsHit == queryParameters.Pins);
+            }
+            if (queryParameters.IsSpare != null)
+            {
+                shots = shots.Where(
+                    s => s.IsSpareShot == queryParameters.IsSpare);
+            }
+            if (queryParameters.Converted != null)
+            {
+                shots = shots.Where(
+                    s => s.WasConverted == queryParameters.Converted);
+            }
+            if (queryParameters.SpareType != null)
+            {
+                shots = shots.Where(
+                    s => s.SpareType == queryParameters.SpareType);
+            }
+
+
+            return Ok(await shots.ToArrayAsync());
+        }
+
+
         [HttpGet("{id}")]
-        public async Task<IActionResult> GetAllShots(int id)
+        public async Task<IActionResult> GetShot(int id)
         {
             var shot = await _context.Shots.FindAsync(id);
             if (shot == null)
