7e7b1e0658e10351a0ae2a6b946c7b1e48b63df0
Azeem Uraizee <aauraizee@gmail.com>
2020-04-28 19:50:25 -0400

    Added Auth controller and register method

---
+++ BowlingAPI/Controllers/AuthController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BowlingAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BowlingAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly BowlingContext _context;
+
+        public AuthController(BowlingContext context)
+        {
+            _context = context;
+
+            _context.Database.EnsureCreated();
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(string username, string password)
+        {
+            username = username.ToLower();
+
+            if (await _context.Players.AnyAsync(p => p.Username == username))
+                return BadRequest("This username already exists!");
+
+            var playerToRegister = new Player
+            {
+                Username = username
+            };
+
+            byte[] passwordHash, passwordSalt;
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+
+            playerToRegister.PasswordHash = passwordHash;
+            playerToRegister.PasswordSalt = passwordSalt;
+
+            await _context.Players.AddAsync(playerToRegister);
+            await _context.SaveChangesAsync();
+
+            return StatusCode(201);
+            
+        }
+
+
+
+    }
+}
