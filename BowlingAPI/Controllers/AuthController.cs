ad1ae188ec622e5144bb9a7a32cc9b509890f934
Azeem Uraizee <aauraizee@gmail.com>
2020-04-28 20:46:44 -0400

    Added login method to auth controller

--- BowlingAPI/Controllers/AuthController.cs
+++ BowlingAPI/Controllers/AuthController.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using BowlingAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace BowlingAPI.Controllers
 {
@@ -15,8 +20,11 @@ namespace BowlingAPI.Controllers
     {
         private readonly BowlingContext _context;
 
-        public AuthController(BowlingContext context)
+        private readonly IConfiguration _config;
+
+        public AuthController(BowlingContext context, IConfiguration config)
         {
+            _config = config;
             _context = context;
 
             _context.Database.EnsureCreated();
@@ -32,7 +40,9 @@ namespace BowlingAPI.Controllers
 
             var playerToRegister = new Player
             {
-                Username = username
+                Username = username,
+                GamesPlayed = 0,
+                CurrentAverage = 0
             };
 
             byte[] passwordHash, passwordSalt;
@@ -50,6 +60,57 @@ namespace BowlingAPI.Controllers
 
             return StatusCode(201);
             
+        }
+
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(string username, string password)
+        {
+            username = username.ToLower();
+            var player = await _context.Players.FirstOrDefaultAsync(p => p.Username == username);
+
+            if (player == null)
+                return Unauthorized();
+
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(player.PasswordSalt))
+            {
+                var comparisonHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                for (int i = 0; i < comparisonHash.Length; i++)
+                {
+                    if (comparisonHash[i] != player.PasswordHash[i])
+                        return Unauthorized();
+                }
+            }
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, player.PlayerId.ToString()),
+                new Claim(ClaimTypes.Name, player.Username)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddMinutes(30),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return Ok(new
+            {
+                token = tokenHandler.WriteToken(token)
+            });
+
+
+
+
         }
 
 
